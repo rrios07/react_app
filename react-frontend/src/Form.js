2e36a9caa44d3591994bfc5e39bc2265acdf7ee0
rrios07 <richardrios7760@gmail.com>
2023-10-01 09:00:44 -0700

    updated Form and MyApp to allow form submission

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -1,7 +1,7 @@
 // src/Form.js
 import React, {useState} from 'react';
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState(
      {
         name: "",
@@ -20,6 +20,10 @@ function Form() {
         {name: value, job: person['job']}   
       );
   }
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({name: '', job: ''});
+  }
   return (
     <form>
       <label htmlFor="name">Name</label>
@@ -36,6 +40,7 @@ function Form() {
         id="job"
         value={person.job}
         onChange={handleChange} />
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
   );
 }
