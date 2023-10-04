ec5270923575e8ab10e0ca39b4ee7e8458411611
rrios07 <richardrios7760@gmail.com>
2023-10-04 16:36:06 -0700

    added post and get support to front end to link frontend and backend

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,19 +1,49 @@
 // src/MyApp.js
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from './Form';
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]); 
+
+  function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+	    .then((res) => res.json())
+	    .then((json) => setCharacters(json["users_list"]))
+	    .catch((error) => { console.log(error); });
+  }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function updateList(person) { 
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+
   function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
 	  return i !== index
       });
     setCharacters(updated);
-  }
-  function updateList(person) {
-    setCharacters([...characters, person]);
   }
   return (
     <div className="container">
@@ -22,6 +52,7 @@ function MyApp() {
         <Form handleSubmit={updateList} />
     </div>  
   )
+
 }
 
 export default MyApp;
